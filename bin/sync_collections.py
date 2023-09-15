5ee515dd75d9d49546b879211d0e277b2240ea6d
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2023-09-15 12:46:53 -0400

    package updates etc

---
+++ bin/sync_collections.py
@@ -0,0 +1,39 @@
+import requests
+import json
+import os
+
+complete = False
+page = 1
+
+query = "https://collections.leventhalmap.org/search?utf8=✓&f_inclusive[collection_name_ssim][]=American+Revolutionary+War-Era+Maps+(Collection+of+Distinction)&format=json"
+
+def parse_collection_record(doc):
+    filename = "../src/content/maps/{}.json".format(doc["id"].replace(":","--"))
+    
+    with open(filename, "w") as f:
+
+        jsonContent = {"argoMetadata": {}, "dcMetadata": doc}
+        json.dump(jsonContent, f)
+
+
+
+
+
+while not complete:
+    print ("Hi")
+    r = requests.get("{}&per_page=100&page={}".format(query, page))
+
+    j = r.json()
+
+    for doc in j["response"]["docs"]:
+        parse_collection_record(doc)
+
+    if j["response"]["pages"]["last_page?"]:
+        complete = True
+    else:
+        page = page + 1
+
+
+
+
+
