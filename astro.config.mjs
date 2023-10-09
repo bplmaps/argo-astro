343f0e03b6ffd861e28b86c069316a84b1a0c1eb
Alex Knoll <alex@agk.io>
2023-10-09 16:04:35 -0400

    migrate page template from Nuxt app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), mdx()]
+  integrations: [vue(), mdx(), react()]
 });
