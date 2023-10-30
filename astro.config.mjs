40b12e588493f30cdd95a609ca61dec98bf7106b
Alex Knoll <alex@agk.io>
2023-10-30 11:41:37 -0400

    add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [vue(), mdx(), react()]
+  integrations: [vue(), mdx(), react(), tailwind()]
 });
-
