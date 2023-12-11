578760450717de4384f4eaeb77d1b10074703edd
Alex Knoll <alex@agk.io>
2023-12-11 13:13:17 -0500

    componentize header/footer and convert to Tailwind utility classes for styling

---
+++ src/components/Layout/Header/MenuItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <a :href="href" class="hover:underline">{{ text }}</a>
+  </li>
+</template>
+
+<script>
+export default {
+	name: 'NavItem',
+  props: {
+    href: String,
+    text: String,
+	},
+}
+</script>
