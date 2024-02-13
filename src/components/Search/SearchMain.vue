cde0f1675f58c80edb769188347e1c62b4be85b5
Garrett Dash Nelson <garrettdashnelson@gmail.com>
2024-02-13 18:08:17 -0500

    search fixes

--- src/components/Search/SearchMain.vue
+++ src/components/Search/SearchMain.vue
@@ -42,7 +42,7 @@ watch(queryString, () => {
 </script>
 
 <template>
-    <form>
+    <form @submit.prevent="performSearch">
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div class="relative">
             <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -55,23 +55,21 @@ watch(queryString, () => {
             <input v-model="queryString" type="search" id="default-search"
                 class="block w-full p-4 ps-10 text-xl text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search keywords, locations, people, institutions ..." required>
-            <button type="submit"
-                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
-        </div>
+             </div>
     </form>
 
     <section>
-        <div class="grid grid-cols-5 gap-4 mt-6">
-        <Card
-            client:visible
-            v-for="r in results"
-            :key="r.id"
-            :subtitle="r.admin_set_name_ssi"
-            :title="r.title_info_primary_tsi"
-            :link="{href: `../maps/${r.id}`, text: 'See map'}"
-            :image="`https://bpldcassets.blob.core.windows.net/derivatives/${r.exemplary_image_key_base_ss}/image_thumbnail_300.jpg`"
-          />
+        <div v-if="queryString.length < 1" class="w-full p-6 text-lg">
+            <h1>Enter a search term</h1>
         </div>
 
-    </section></template>
+        <div class="grid grid-cols-2 md:grid-cols-5 gap-4 mt-6">
+
+            <Card client:visible v-for="r in results" :key="r.id" :subtitle="r.admin_set_name_ssi"
+                :title="r.title_info_primary_tsi" :link="{ href: `../maps/${r.id}`, text: 'See map' }"
+                :image="`https://bpldcassets.blob.core.windows.net/derivatives/${r.exemplary_image_key_base_ss}/image_thumbnail_300.jpg`" />
+        </div>
+
+    </section>
+</template>
 
