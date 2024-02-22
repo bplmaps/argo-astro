c4a71ad87192d799998bb39a9d89809c57b6d35d
Alex Knoll <alex@agk.io>
2024-02-22 07:47:00 -0500

    clean up spacing/indents

--- src/components/Maps/Allmaps.vue
+++ src/components/Maps/Allmaps.vue
@@ -1,23 +1,19 @@
 <template>
-    <h2 class="text-xl mb-1">Georeferencing</h2>
-    <div class="flex flex-wrap gap-x-1 gap-y-2">
-        <a :href=editorURL target="_blank" class="button arrow">Georeference this Map</a>
-    </div>
+  <h2 class="text-xl mb-1">Georeferencing</h2>
+  <div class="flex flex-wrap gap-x-1 gap-y-2">
+    <a :href=editorURL target="_blank" class="button arrow">Georeference this Map</a>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-    props: {
-        manifestId: String
-    },
-    computed: {
-        editorURL() {
-            return `https://editor.allmaps.org/#/collection?url=${encodeURIComponent(this.manifestId)}`;
-        }
+  props: {
+    manifestId: String
+  },
+  computed: {
+    editorURL() {
+      return `https://editor.allmaps.org/#/collection?url=${encodeURIComponent(this.manifestId)}`;
     }
+  }
 }
-
-
 </script>
