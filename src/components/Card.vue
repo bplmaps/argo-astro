e12f6e169caef83c72a8f05bd151cad240b7e4f4
Alex Knoll <alex@agk.io>
2024-03-04 08:03:26 -0500

    font weight, line height, and spacing cleanup

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -11,9 +11,11 @@
         class="absolute z-10 inset-0 w-full h-full object-center object-cover"
       />
     </div>
-    <div class="relative w-full flex flex-grow flex-col items-start p-3">
-      <h3 v-if="title" class="text-lg leading-tight font-medium overflow-hidden text-ellipsis line-clamp-2 mb-2">{{title }}</h3>
-      <p v-if="subtitle" class="text-base leading-tight font-light overflow-hidden text-ellipsis line-clamp-3 mb-3">{{ subtitle }}</p>
+    <header class="relative w-full flex flex-grow flex-col items-start px-3 pt-3 pb-4">
+      <h3 v-if="title" class="text-lg leading-tight font-semibold overflow-hidden text-ellipsis text-bismark-900 line-clamp-2">{{title }}</h3>
+      <p v-if="subtitle" class="text-base leading-tight font-light overflow-hidden text-ellipsis line-clamp-3">{{ subtitle }}</p>
+    </header>
+    <div class="px-3 pb-3">
       <a class="button dark mt-auto" :href="link.href">{{ link.text }}</a>
     </div>
   </div>
